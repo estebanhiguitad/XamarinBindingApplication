4d9a139af7ac7356c8d2f0780df962ceb6aceaaa
David Santacoloma <adsi1407@gmail.com>
2021-07-22 11:04:34 -0500

    Bind an empty Android application

---
+++ EmptyAndroidApplication/app/src/main/java/co/com/ceiba/emptyapplication/FromBindingActivity.kt
@@ -0,0 +1,11 @@
+package co.com.ceiba.emptyapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FromBindingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_from_binding)
+    }
+}
